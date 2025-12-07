b31327452b40e66488c08631c2f1ea98ecd04b9e
Vibush01 <vibush01@gmail.com>
2025-12-07 12:41:25 +0530

    Changes for the deployment

--- server/server.js
+++ server/server.js
@@ -9,10 +9,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins for simplicity in deployment
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
-mongoose.connect( process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
